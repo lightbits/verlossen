8e080414ffc069a5e67b7c761f936c1da3b73464
lightbits <simenh.jobb@gmail.com>
2015-01-21 23:28:57 +0100

    Client/server comm working

--- main.cpp
+++ main.cpp
@@ -3,53 +3,89 @@
 #include "win32_net.cpp"
 #include <windows.h>
 
-int   CL_LISTEN_PORT = 12345;
-int   SV_TICKRATE = 20;
-int   CL_CMDRATE = 20;
-int   CL_UPDATERATE = 20;
-int   SV_LISTEN_PORT = 65387;
-int   SV_MAX_CONNECTIONS = 8;
-int   SV_TIMEOUT_SECONDS = 10;
-float CL_INTERP = 0.01f;
+const int   CL_LISTEN_PORT = 12345;
+const int   SV_TICKRATE = 20;
+const int   CL_CMDRATE = 20;
+const int   CL_UPDATERATE = 20;
+const int   CL_PACKET_SIZE = 64;
+const int   SV_PACKET_SIZE = 64;
+const int   SV_LISTEN_PORT = 65387;
+const int   SV_LISTEN_IP0  = 127;
+const int   SV_LISTEN_IP1  = 0;
+const int   SV_LISTEN_IP2  = 0;
+const int   SV_LISTEN_IP3  = 1;
+const int   SV_MAX_CONNECTIONS = 8;
+const int   SV_TIMEOUT_SECONDS = 10;
+const float CL_INTERP = 0.01f;
+
+// Message types
+const uint32 APP_PROTOCOL   = 0xDEADBEEF;
+const uint32 APP_CONNECT    = 0x00000001;
+const uint32 APP_DISCONNECT = 0x00000002;
 
 struct ConnectionResponse
 {
-    uint32 welcome_length;
-    char welcome[255];
+    uint32 protocol;
+    char welcome[SV_PACKET_SIZE - sizeof(uint32)];
+};
+
+struct Acknowledge
+{
+    uint32 protocol;
+    uint32 regards;
 };
 
 struct ConnectionRequest
 {
     uint32 protocol;
+    uint32 connect;
+    char   hail[CL_PACKET_SIZE - 2 * sizeof(uint32)];
 };
 
 ConnectionResponse
 ClientConnect(NetAddress *server)
 {
     ConnectionRequest request = {};
-    request.protocol = 0xdeadbeef;
-    NetAddress dst = { 127, 0, 0, 1, SV_LISTEN_PORT };
+    request.protocol = APP_PROTOCOL;
+    request.connect  = APP_CONNECT;
 
-    while (1)
-    {
-        NetSend(&dst, (const char*)&request, sizeof(ConnectionRequest));
-        Sleep(1000);
-    }
+    // TODO: Replace with safer snprintf
+    sprintf(request.hail, "Yo wassup!");
+    NetAddress dst = { 
+        SV_LISTEN_IP0, 
+        SV_LISTEN_IP1, 
+        SV_LISTEN_IP2, 
+        SV_LISTEN_IP3, 
+        SV_LISTEN_PORT 
+    };
 
-    while (!NetSend(&dst, (const char*)&request, sizeof(ConnectionRequest)))
-    {
-        // Wait...
-    }
-
-    // Ok now wait for response
     ConnectionResponse response = {};
-    // uint32 read_bytes = NetRead(
-    //     (uint8*)&response, sizeof(ConnectionResponse), server);
-    // while (read_bytes < sizeof(ConnectionResponse))
-    // {
-    //     read_bytes = NetRead(
-    //         (uint8*)&response, sizeof(ConnectionResponse), server);
-    // }
+    int read_bytes = 0;
+    int max_read_tries = 5;
+    printf("Attempting to connect to %d.%d.%d.%d:%d\n", 
+        dst.ip0, dst.ip1, dst.ip2, dst.ip3, dst.port);
+    while (read_bytes <= 0)
+    {
+        // Send our connection request!
+        NetSend(&dst, (char*)&request, sizeof(ConnectionRequest));
+        
+        int read_tries = 0;
+        bool valid = false;
+        while (read_bytes <= 0 && read_tries < max_read_tries && !valid)
+        {
+            read_bytes = NetRead(
+                (char*)&response, sizeof(ConnectionResponse), server);
+
+            if (response.protocol == APP_PROTOCOL)
+                valid = true;
+
+
+
+            read_tries++;
+            Sleep(1000);
+        }
+        printf("Retrying connection\n");
+    }
     return response;
 }
 
@@ -62,38 +98,14 @@ ClientDisconnect(NetAddress *server)
 int 
 main(int argc, char **argv)
 {
-    // This works!
+    NetAddress server = {};
     NetSetPreferredListenPort(CL_LISTEN_PORT);
-    const char *data = "HELLO WORLD";
-    NetAddress dst = { 127, 0, 0, 1, SV_LISTEN_PORT };
-    while (1)
-    {
-        int sent_bytes = NetSend(&dst, data, 12);
-        printf("Sent %d bytes\n", sent_bytes);
-        Sleep(1000);
-    }
-    ////////////////////////////////////////////////
-
-    // This works!
-    // NetSetPreferredListenPort(12345);
-    // NetAddress sender = {};
-    // char data[256];
-    // int bytes_read = NetRead(data, 256, &sender);
-    // while (bytes_read <= 0)
-    // {
-    //     bytes_read = NetRead(data, 256, &sender);
-    // }
-    // printf("Read %d bytes\n", bytes_read);
-    // Sleep(5000);
-    ////////////////////////////////////////////////
-
-    // NetAddress server = {};
-    // NetSetPreferredListenPort(CL_LISTEN_PORT);
-    // ConnectionResponse response = ClientConnect(&server);
-    // printf("Connection established\n");
-    // printf("sv port: %d\n", server.port);
-    // printf("welcome: %s\n", response.welcome);
-    // ClientDisconnect(&server);
+    ConnectionResponse response = ClientConnect(&server);
+    printf("Connection established\n");
+    printf("sv port: %d\n", server.port);
+    printf("welcome: %s\n", response.welcome);
+    ClientDisconnect(&server);
     NetClose();
+    Sleep(5000);
     return 0;
 }
--- server.go
+++ server.go
@@ -1,101 +1,147 @@
+/*
+http://stackoverflow.com/questions/12854125/go-how-do-i-dump-the-struct-into-the-byte-array-without-reflection
+http://stackoverflow.com/questions/16330490/in-go-how-can-i-convert-a-struct-to-a-byte-array
+*/
+
 package main
 
 import (
-    ."fmt"
+    "log"
     "net"
+    "bytes"
+    "encoding/binary"
     "time"
 )
 
-// Do we need multiple ports to transmit world updates?
-// Or can we just send everything over 27050?
-// Sending updates must be done towards each connection
-// A good use case for go threads!
-// The threads might timeout, in which case we drop the client
+// TODO: Import these symbols from C header files
+const APP_PROTOCOL = 0xDEADBEEF
+const APP_CONNECT  = 0x00000001
+const APP_PACKET_SIZE = 64
+const APP_PROTOCOL_SIZE = 4
+const APP_APP_SIZE = 4
+const APP_HEADER_SIZE = APP_PROTOCOL_SIZE + APP_APP_SIZE
+const APP_HAIL_SIZE = APP_PACKET_SIZE - APP_HEADER_SIZE
 
-// struct ClientState
-// {
-//     talk // words they wish to send to world
-//     has_spoken // whether we have sent these words yet
-// };
+const SV_PACKET_SIZE = 64
+const SV_LISTEN_ADDRESS = "127.0.0.1:65387"
+const SV_CLIENT_RETRY_INTERVAL = 1 * time.Second
+const SV_CLIENT_MAX_RETRIES = 10
 
-// type Client struct {
-//     address net.UDPAddr
-// }
+type Connection struct {
+    Address  string
+    // TimedOut bool
+    // LastPacketReceived time
+}
 
-func SendTest() {
-    socket, err := net.Dial("udp", "127.0.0.1:20750")
+/*
+Instead of the server requiring an explicit ACK from the client after
+sending data, I'll go for a scheme where the client just continually
+pushes data to the server. 
+
+Since this is a gaming application, clients are probably going to send
+data quite often. It seems reasonable that we can include a pinging
+operation as well. That is, have the client send a "Hey, I'm alive!"
+to the server every once in a while. If it goes too long before the 
+server receives such a message, the client times out and is dropped.
+*/
+
+var SV_CONNECTIONS map[string]Connection
+
+func SendToClient(who Connection, data []byte) {
+    // Should all clients always be sending to SV_LISTEN_PORT, or should they
+    // get their own designated port when they send a connection request
+    // to the server?
+
+    // This creates a new socket on the server with a different port each time
+    // i.e. does not do the above.
+    socket, err := net.Dial("udp", who.Address)
     defer socket.Close()
     if err != nil {
-        Println("Failed to create UDP socket")
+        log.Println("Failed to create UDP socket")
     }
 
-    for {
-        time.Sleep(1*time.Second)
-        
-        sent_bytes, err := socket.Write([]byte("HELLO WORLD\n"))
-        if err != nil {
-            Println("Failed to send data")
-        }
-        Printf("Sent %d bytes\n", sent_bytes)
+    sent_bytes, err := socket.Write(data)
+    if err != nil {
+        log.Println("Failed to send data to", who.Address, "with", data)
+    }
+    log.Println("Sent", sent_bytes, "to", who.Address)
+
+    // We do not wait for an explicit ack back from the client
+    // If he misses this data, too bad!
+}
+
+func AcceptIncomingConnection(who string, data *bytes.Reader) {
+    // Read hail message (yes, all this code is for that)
+    // TODO: Find a better way to do this
+    hail_bytes := make([]byte, APP_HAIL_SIZE)
+    _, err := data.Read(hail_bytes)
+    if err != nil {
+        log.Println("Failed connection from", who)
+    }
+    hail_length := bytes.Index(hail_bytes, []byte{0})
+    hail := string(hail_bytes[:hail_length])
+    log.Println(who, "wishes to connect:", hail)
+
+    // Add client to list of active connections
+    connection := Connection{who}
+    SV_CONNECTIONS[who] = connection
+
+    // Send response
+    type Response struct {
+        Protocol uint32
+        Welcome  [APP_PACKET_SIZE - APP_PROTOCOL_SIZE]byte
     }
 
+    response := Response{Protocol: APP_PROTOCOL}
+    copy(response.Welcome[:], []byte("Hello... you!"))
+    blob := new(bytes.Buffer)
+    binary.Write(blob, binary.LittleEndian, response)
 
+    SendToClient(connection, blob.Bytes())
 }
 
 func main() {
-    const SV_LISTEN_ADDRESS = "127.0.0.1:65387"
-
     address, err := net.ResolveUDPAddr("udp", SV_LISTEN_ADDRESS)
     if err != nil {
-        Println("Failed to resolve UDP address")
+        log.Fatalln("Failed to resolve UDP address", err)
     }
 
     socket, err := net.ListenUDP("udp", address)
     defer socket.Close()
     if err != nil {
-        Println("Failed establish UDP listening thingy?")
+        log.Fatalln("Failed create UDP socket", err)
     }
 
-    // go SendTest()
+    SV_CONNECTIONS = make(map[string]Connection)
 
-    var buf []byte = make([]byte, 1024)
     for {
-        read_bytes, sender, err := socket.ReadFromUDP(buf)
+        packet := make([]byte, APP_PACKET_SIZE)
+        read_bytes, sender, err := socket.ReadFromUDP(packet)
         if err != nil {
-            Println("Something went wrong while reading from UDP")
-            break
+            log.Println("Something went wrong while reading from UDP", err)
+            continue
         }
-        Println("Read", read_bytes, "from", sender)
+
+        var protocol uint32
+        var regards uint32
+        reader := bytes.NewReader(packet)
+        err = binary.Read(reader, binary.LittleEndian, &protocol)
+        err = binary.Read(reader, binary.LittleEndian, &regards)
+        if err != nil {
+            log.Println("Something went wrong while parsing packet", err)
+        }
+
+        log.Println("Read", read_bytes, "from", sender, protocol)
+        if protocol != APP_PROTOCOL {
+            log.Println("Received a packet with an unknown protocol")
+            continue
+        }
+
+        who := sender.String()
+
+        switch regards {
+        case APP_CONNECT:
+            AcceptIncomingConnection(who, reader)
+        }
     }
-
-    // for {
-    //     select {
-    //     case <-client_recv:
-    //         // Received data from client
-    //         // Update client state
-
-    //     case <-world_update:
-    //         // Update world and send changes to all connections
-    //         // Includes transmitting text
-    //     }
-    // }
-
-    // This works!
-    // socket, err := net.Dial("udp4", "127.0.0.1:12345")
-    // defer socket.Close()
-    // if err != nil {
-    //     Println("Failed to create UDP socket")
-    // }
-
-    // Println("Got local address", socket.LocalAddr())
-
-    // for {
-    //     time.Sleep(1*time.Second)
-        
-    //     sent_bytes, err := socket.Write([]byte("HELLO WORLD\n"))
-    //     if err != nil {
-    //         Println("Failed to send data")
-    //     }
-    //     Printf("Sent %d bytes\n", sent_bytes)
-    // }
 }
